9b5bb06a98dcd1658705052f7f3f746e3a3d2f6b
matee911 <matee@matee.net>
2014-11-13 09:47:57 +0100

    Update list_on_class.py

--- random_snippets/list_on_class.py
+++ random_snippets/list_on_class.py
@@ -13,3 +13,16 @@ print Foo.events    # => [1]
 foo.events = []
 print foo.events    # => []
 print Foo.events    # => [1]
+
+foo.events.append(2)
+print foo.events    # => [2]
+print Foo.events    # => [1]
+
+foo = Foo()
+foo.events.append(3)
+print foo.events    # => [1, 3]
+print Foo.events    # => [1, 3]
+
+foo.events[:] = []
+print foo.events    # => []
+print Foo.events    # => []
