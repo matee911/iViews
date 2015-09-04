2e4a13cc5e4e41de79da939c2264a138b0ada7e9
matee <matee@matee.net>
2015-09-04 13:39:04 +0200

    obfuscation example

---
+++ obfuscation.java
@@ -0,0 +1,7 @@
+public class Puzzle {
+        public static void main(String[] args) {
+                String a = "1";
+                String b = "\u0022\u003b\u0061\u003d\u0022\u0032\u0022\u003b\u002f\u002f";    // u'";a="2";//'
+                System.out.println(a);
+        }
+}
